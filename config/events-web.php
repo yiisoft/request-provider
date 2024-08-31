2feb1ac9102f65843072f0f5a5d2680bf6d49334
Dmitrii Derepko <xepozz@list.ru>
2024-08-31 17:38:25 +0300

    Clean file

--- config/events-web.php
+++ config/events-web.php
@@ -2,16 +2,14 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
 use Yiisoft\Middleware\Dispatcher\Event\BeforeMiddleware;
-use Yiisoft\RequestProvider\RequestProvider;
 use Yiisoft\RequestProvider\RequestProviderInterface;
 
 /** @var array $params */
 
 return [
     BeforeMiddleware::class => [
-        static function (ContainerInterface $container, RequestProviderInterface $provider, BeforeMiddleware $event) {
+        static function (RequestProviderInterface $provider, BeforeMiddleware $event) {
             $provider->set($event->getRequest());
         },
     ],
